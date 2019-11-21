76552cb036f01155d8d156c865fad939fcaf7ee6
jmais <joseph.maisevich@ufl.edu>
2019-11-20 22:04:57 -0500

    Added  components for news page and article

---
+++ src/components/Article.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Article;
---
+++ src/components/News.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>News Site</h1>  );
+    }
+}
+ 
+export default News;
