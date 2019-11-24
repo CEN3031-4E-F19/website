00e7b9deb1cfc24343a9b7f5e268f39c721c6d80
jmais <joseph.maisevich@ufl.edu>
2019-11-24 10:35:58 -0500

    setting data as a state variable in article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class Article extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { data:[] }
+        this.getData = this.getData.bind(this);
+    }
+    getData(){
+        axios.get('api/articleScrape').then((req,res)=>{
+            this.setState({data:req.data});
+        });
+        console.log(this.state);
     }
     render() { 
-        return (  );
+        return ( <h1>news article</h1> );
     }
 }
  
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Article from './Article';
 
 
 class News extends Component {
@@ -8,8 +8,7 @@ class News extends Component {
         this.state = {  }
     }
     render() { 
-        axios.get('api/articleScrape');
-        return ( <h1>News Site</h1>  );
+        return ( <Article/>);
     }
 }
  
