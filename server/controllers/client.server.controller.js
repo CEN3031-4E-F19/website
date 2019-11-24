bcb5761b08325c1a4f59bec9a8f070e4d8e19944
Dana G <32311532+dastryyy@users.noreply.github.com>
2019-11-24 14:29:40 -0500

    added delete functionality to server controller

--- server/controllers/client.server.controller.js
+++ server/controllers/client.server.controller.js
@@ -15,3 +15,27 @@ exports.create = function(req, res) {
         );
     }
 }
+
+exports.read = function(req, res) {
+    res.json(req.client);
+}
+
+exports.update = function(req, res) {
+    var client = req.client;
+    //replace properties with properties found in req.body
+
+    //save
+}
+
+exports.delete = function(req, res) {
+    var client = req.client;
+    Client.findOneAndDelete({_id: client.id}, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        } else {
+            res.json(client);
+            console.log('Deleted');
+        }
+    });
+}
