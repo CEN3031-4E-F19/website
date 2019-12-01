eb66090ddb31edfbd48dace9418791eb4d86aa87
kukkadamsai <kukkadamsai@yahoo.com>
2019-12-01 16:02:50 -0500

    Added a picture to the home page. Added information about the companies mission statement to the home page as well

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import water from "./waterHand.png"
 
-//test (Dana Graham)
 
 function Home() {
   return (
     <div className="text-center">
-      <h1>Home Page</h1>
+     
+      <img className="waterHand" src={water}  />
+
+      <div class="bottom-left">Innovative solutions for clean water strategies</div>
+      <div class="bottom-right">NiekAab Desal’s vision is to provide families and businesses with affordable and sustainable tools to ensure safe and reliable drinking water.</div>
+      <div class="bottom">In NiekAab Desal, our mission is to be a leading design, manufacturing, and technical service firm delivering an end-to-end solution for water pollution.</div>
     </div>
   );
 }
+
 
 export default Home;
