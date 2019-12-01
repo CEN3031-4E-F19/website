384421dfbe7a6dc98c899de19a0497bd4db4194f
jmais <joseph.maisevich@ufl.edu>
2019-12-01 16:17:31 -0500

    Splits articles into pages 10 per page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card'
-import axios from 'axios';
 
 
 class Article extends Component {
     constructor(props) {
         super(props);
-        this.state = { data:[] }
+        this.state = {}
     }    
-    componentDidMount(){
-        axios.get('api/articleScrape').then((req,res)=>{
-            this.setState({data:req.data});
-        });
-    }
+    
     render() { 
-        console.log(this.state);
-        const articleList = this.state.data.map((article,i)=>{
+        var endIndex = this.props.indexStart*10-1>this.props.data.length?this.props.data.length:this.props.indexStart*10;
+        var indexbegin = this.props.indexStart==1?0:this.props.indexStart*10-10;
+        const articleList = this.props.data.slice(indexbegin,endIndex).map((article,i)=>{
             return(
             <div className="row" key={i}>
                 <Card style = {{width: '85rem'}} className="mx-auto shadow-sm p-3 mb-2 bg-white rounded " >
                 <Card.Title>{article.title}</Card.Title>
-                <Card.Body>{article.desc}</Card.Body>
+                <Card.Body>{article.description}</Card.Body>
                 <a target='_blank' href={article.link} className="stretched-link"/>
                 </Card>
             </div>
