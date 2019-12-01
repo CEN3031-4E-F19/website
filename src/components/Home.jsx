58efb230b1677e7e0146c26412f3408ca83f0fb1
kukkadamsai <kukkadamsai@yahoo.com>
2019-12-01 17:18:09 -0500

    Added more pictures to the home page. Added shadows to pictures. Added information about water.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import water from "./waterHand.png"
+import drought from "./Drought.png"
+import drought1 from "./drought.1.png"
+import pipe from "./pipe.png"
+import pipe1 from "./pipe.1.png"
 
 
 function Home() {
@@ -11,6 +15,13 @@ function Home() {
       <div class="bottom-left">Innovative solutions for clean water strategies</div>
       <div class="bottom-right">NiekAab Desal’s vision is to provide families and businesses with affordable and sustainable tools to ensure safe and reliable drinking water.</div>
       <div class="bottom">In NiekAab Desal, our mission is to be a leading design, manufacturing, and technical service firm delivering an end-to-end solution for water pollution.</div>
+      <img className="Drought" src={drought} />  
+      <div class="droughtInfo">"By 2025, Half of the world's population will be living in water-stressed areas."
+      -World Health Organization</div>
+      <img className="pipe" src={pipe} />
+      <div class="pipeInfo">"Nearly 1/3  of U.S. water systems reported service lines containing lead."
+      -American Association for the Advancement of Science</div>
+
     </div>
   );
 }
