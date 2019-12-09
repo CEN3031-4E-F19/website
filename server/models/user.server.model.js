6f8cf0c30f7b53630469364284e47c1c94d3ea18
Dana G <32311532+dastryyy@users.noreply.github.com>
2019-12-09 05:58:32 -0500

    added methods to schema

--- server/models/user.server.model.js
+++ server/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'), 
+    bcrypt = require('bcrypt.js'),
     Schema = mongoose.Schema;
 
 
@@ -6,6 +7,14 @@ var mongoose = require('mongoose'),
         username:String,
         password:String
       });
+
+      userSchema.methods.generateHash = function(password) {
+        return bcrypt.hashSync(password,bcrypt.genSaltSync(10),null);
+      };
+
+      userSchema.methods.validatePassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+      };
 
 
       var User = mongoose.model('User', userSchema);
