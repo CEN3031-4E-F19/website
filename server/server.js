f33c1bbd5f162c3a6680911191ac49d7982fb45c
jmais <joseph.maisevich@ufl.edu>
2019-12-09 19:30:20 -0500

    Maybe works

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('../server/config/express.js')
+passport = require('passport')
+path = require('path')
+const app = express.init()
 
- 
+app.get('/Admin', passport.authenticate('jwt', { session: false }),(req,res)=>{
+
+    req.sendFile(path.join(__dirname+'/client/build/index.html'));
+  }
+  
+);
 // Use env port or default
 const port = process.env.PORT || 5000;
 
-const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
